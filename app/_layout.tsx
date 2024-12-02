2dc325768f3d5f45e33dab8ebe0707c7ef10e138
adrian <acedillo99@gmail.com>
2024-12-02 14:08:34 -0600

    esqueleto terminado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'ClashDisplay': require('../assets/fonts/ClashDisplay-Regular.otf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack screenOptions={{ 
       headerShown: false,
@@ -8,6 +25,7 @@ export default function RootLayout() {
         backgroundColor: 'transparent'
       }
     }}>
+      <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
     </Stack>
   );
